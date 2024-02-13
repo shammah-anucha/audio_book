ae901b0846934748287d70bf4444f6aba599eb3b
Shammah Anucha <shammah.anucha@yahoo.com>
2024-02-13 15:08:37 +0100

    bind service

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from starlette.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 
@@ -23,3 +24,7 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+if __name__ == "__main__":
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
