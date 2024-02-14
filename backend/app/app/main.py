a29992ddb72c1e35bebb3aab6290872d04bd3851
Shammah Anucha <shammah.anucha@yahoo.com>
2024-02-14 22:59:02 +0100

    fixed routers

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -6,13 +6,6 @@ from sqlalchemy.orm import Session
 from ..app.api.api_v1.api import api_router
 from .core.config3 import settings
 
-from fastapi import APIRouter, HTTPException, Depends
-from typing import Any, List
-from sqlalchemy.orm import Session
-from ..app.schemas.users import User, UserCreate
-from ..app.models import models
-from ..app.api import deps
-from .crud import crud_users
 
 # main.py
 
@@ -20,9 +13,6 @@ from .crud import crud_users
 app = FastAPI(
     title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}/openapi.json"
 )
-# app = FastAPI(
-#     title=settings.PROJECT_NAME, openapi_url="https://audio-book-api-m825.onrender.com"
-# )
 
 # Set all CORS enabled origins
 if settings.BACKEND_CORS_ORIGINS:
@@ -35,13 +25,3 @@ if settings.BACKEND_CORS_ORIGINS:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
-
-
-# @app.get("/")
-# def read_root():
-#     return "Hello World"
-
-
-# if __name__ == "__main__":
-
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
