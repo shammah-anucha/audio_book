f55d6dc4b75085675b649ed2117237b44b1166f4
Shammah Anucha <shammah.anucha@yahoo.com>
2024-02-14 07:26:44 +0100

    reverse route

--- backend/app/app/core/config3.py
+++ backend/app/app/core/config3.py
@@ -12,9 +12,9 @@ SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 class Settings(BaseSettings):
     # development
-    # API_V1_STR: str = "/api/v1"
+    API_V1_STR: str = "/api/v1"
     # production
-    API_V1_STR: str = "/ HTTP/1.1"
+    # API_V1_STR: str = "/ HTTP/1.1"
     SECRET_KEY = secrets.token_urlsafe(32)
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24 * 8
